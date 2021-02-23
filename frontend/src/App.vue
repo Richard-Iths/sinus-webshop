34ee3d544afe0bd67a220d2e2ce667abf239b52e
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-02-23 15:24:53 +0100

    Add global font styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,10 +4,28 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Cantarell:ital,wght@0,400;0,700;1,400;1,700&display=swap");
 
+h1,
+h2,
+h3,
+h4,
+a,
+button {
+  font-family: "Fjalla One", sans-serif;
+  text-decoration: none;
+  color: #000;
+}
+
+p, 
+input{
+  font-family: "Cantarell", sans-serif;
+  color: #000;
+}
 </style>
