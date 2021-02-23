9df45656e70a6cd8af3fe8db0d7ba4d51a930f2d
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-02-23 17:23:23 +0100

    Add modal component

---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="modal" @click="close">
+      <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+    methods: {
+        close(e) {
+            if(e.target.classList.contains('modal')){
+                this.$emit('closeModal')
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal{
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: grid;
+    place-items: center;
+
+    background-color: rgba(0, 0, 0, 0.7);
+}
+</style>
