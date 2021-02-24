baec35901bd2f8346e268e9d3e8cd394516447ff
richard-iths <richard.forsberg@iths.se>
2021-02-24 11:01:51 +0100

    added getters to get user,token,order history and order history by id

--- frontend/src/store/user/index.js
+++ frontend/src/store/user/index.js
@@ -4,10 +4,16 @@ import {userConstants as Mutations} from '@/store/constants'
 const user = {
   state:() => ({
     user:{},
+    userHistoryById = {},
     token:'',
   }
 ),
-getters:{},
+getters:{
+  getUser: (state) => state.user,
+  getUserToken: (state) => state.token,
+  getUserHistory: (state) => state.user.items,
+  getUserHistoryById: (state) => (id) => state.userHistoryById[id]
+},
 mutations:{},
 actions:{},
 },
