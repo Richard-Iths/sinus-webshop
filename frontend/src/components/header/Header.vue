6c87629bd24874ddea831e7c1a6c271bc9edc277
richard-iths <richard.forsberg@iths.se>
2021-02-24 13:36:54 +0100

    added components for nav,header,search and logo

---
+++ frontend/src/components/header/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <logo />
+    <nav />
+  </header>
+</template>
+<script>
+import Logo from "./Logo";
+import Nav from "@/components/nav/Nav.vue";
+export default {
+  components: { Logo, Nav },
+};
+</script>
+<style lang="scss" scoped></style>
