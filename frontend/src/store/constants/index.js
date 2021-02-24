bf6ed976dc7a2fefe2646a27003e53ccfb8546a4
richard-iths <richard.forsberg@iths.se>
2021-02-24 11:53:17 +0100

    added mutations and actions to login,update user profile and update user order history

--- frontend/src/store/constants/index.js
+++ frontend/src/store/constants/index.js
@@ -2,4 +2,5 @@ export const userConstants = {
   LOGIN: "login",
   PATCH_USER: "patchUser",
   UPDATE_USER: "updateUser",
+  UPDATE_ORDER_HISTORY: "updateOrderHistory",
 };
--- frontend/src/store/user/index.js
+++ frontend/src/store/user/index.js
@@ -1,21 +1,66 @@
 import {userConstants as Mutations} from '@/store/constants'
-
+import * as API from '@/api'
 
 const user = {
   state:() => ({
     user:{},
-    userHistoryById = {},
+    userOrderHistory: [],
+    userOrderHistoryById = {},
     token:'',
   }
 ),
 getters:{
   getUser: (state) => state.user,
   getUserToken: (state) => state.token,
-  getUserHistory: (state) => state.user.items,
-  getUserHistoryById: (state) => (id) => state.userHistoryById[id]
+  getUserOrderHistory: (state) => state.user.items,
+  getUserOrderHistoryById: (state) => (id) => state.userOrderHistoryById[id]
 },
-mutations:{},
-actions:{},
+mutations:{
+  [Mutations.LOGIN]:(state,data) => {
+    state.userOrderHistory = data.items;
+    state.user = data.user;
+    state.token = data.token;
+
+    if(data.items.length !== 0) {
+    data.items.forEach(order => {
+      state.userHistoryById[order.id] = order;
+    })
+  }
+  },
+  [Mutations.UPDATE_USER]: (state,user) => {
+      state.user = user;
+  },
+  [Mutations.UPDATE_ORDER_HISTORY]: (state,order) => {
+    state.userOrderHistory.push(order);
+    state.userOrderHistoryById[order.id] = order
+  }
+},
+actions:{
+  login: async ({commit},userObj) => {
+    try {
+      const userData = await API.loginUser(userObj);
+      const orderHistory = await API.getUserOrders(data.token);
+      const data = {...userData,...orderHistory};
+      commit(Mutations.LOGIN,data);
+    } catch (error) {
+      return error.message;
+    }
+
+  },
+  updateUser: async ({commit,getters},userObj) => {
+    try {
+        const token = getters.getUserToken;
+        await API.patchUser(userObj,token)
+        const userUpdate = await API.getUserProfile(token);
+        commit(Mutations.UPDATE_USER);
+    } catch (error) {
+      return error.message
+    }
+  },
+  updateOrderHistory: async ({commit},order) =>{
+    commit(Mutations.UPDATE_ORDER_HISTORY,order);
+  }
+},
 },
 
 
