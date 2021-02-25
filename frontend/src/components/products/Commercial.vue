a6e11a5407ff449280301f693e2e77a7ce8ac93e
richard-iths <richard.forsberg@iths.se>
2021-02-25 14:14:40 +0100

    Added commercial component for diffirent commercials in store and finnished the ladning page

---
+++ frontend/src/components/products/Commercial.vue
@@ -0,0 +1,92 @@
+<template>
+  <section
+    class="commercial"
+    :style="{
+      'background-image': 'url(' + getImage + ')',
+    }"
+  >
+    <div class="commercial__inner">
+      <slot></slot>
+      <Button :value="btnText" />
+    </div>
+  </section>
+</template>
+<script>
+import Button from "@/components/Button.vue";
+export default {
+  props: {
+    btnText: {
+      type: String,
+      required: true,
+    },
+    styleObj: {
+      type: Object,
+      required: true,
+      image: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  components: { Button },
+
+  computed: {
+    getImage() {
+      return require(`@/assets/${this.styleObj.image}`);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.commercial {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  max-height: 100vh;
+  h2,
+  h4 {
+    color: #fff;
+  }
+
+  &__inner {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    min-height: 72vh;
+    background-color: rgba(0, 0, 0, 0.5);
+
+    button {
+      min-width: 25rem;
+      margin: 3rem 2rem;
+      display: block;
+      align-self: flex-end;
+      padding: 2rem;
+      background-color: #222;
+      border: 5px solid #fff;
+      font-size: 1.2em;
+      color: #fff;
+    }
+  }
+  &__sale-container {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: 0 auto;
+    width: 80%;
+    border: 5px solid #c4c0bf;
+    padding: 2rem;
+    h4 {
+      align-self: center;
+    }
+    h2 {
+      &:last-of-type {
+        align-self: flex-end;
+      }
+    }
+  }
+  &__sale-text {
+    color: #f5f560;
+  }
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,61 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+  <section class="home">
+    <Commercial btnText="SHOW ME!" :styleObj="sale">
+      <div class="commercial__sale-container">
+        <h2><span class="commercial__sale-text">NEW COLLECTIONS</span></h2>
+        <h4>In our</h4>
+        <h2>
+          <span class="commercial__sale-text">{{ sale.saleText }} SALE</span>
+        </h2>
+      </div>
+    </Commercial>
+    <Commercial btnText="LET ME SEE!" :styleObj="newStuff">
+      <div class="commercial__sale-container">
+        <h2>
+          <span class="commercial__sale-text">NEW {{ newStuff.saleText }}</span>
+        </h2>
+        <h4>In our</h4>
+        <h2>
+          <span class="commercial__sale-text">INVENTORY</span>
+        </h2>
+      </div>
+    </Commercial>
+    <Commercial btnText="MATCH ME!" :styleObj="match" class="bg-darker">
+      <div class="commercial__sale-container">
+        <h2><span class="commercial__sale-text">MATCH GEAR</span></h2>
+        <h4>After your own taste with our awesome</h4>
+        <h2>
+          <span class="commercial__sale-text">MATCHING POSSIBILITIES</span>
+        </h2>
+      </div>
+    </Commercial>
+  </section>
 </template>
 
 <script>
+import Commercial from "@/components/products/Commercial";
 export default {
   name: "Home",
+  components: { Commercial },
+  data() {
+    return {
+      sale: {
+        image: "ralph-ravi-kayden-MnxiW9KinLA-unsplash.jpg",
+        saleText: "SPRING",
+      },
+      newStuff: {
+        image: "serenity-mitchell-NH2LVXsuGPg-unsplash.jpg",
+        saleText: "BOARDS",
+      },
+      match: {
+        image: "skate-hero-1.jpg",
+      },
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+}
+</style>
