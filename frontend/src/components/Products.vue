22b203b4c49c616e8d3bdd61312b87e9434a2374
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-02-25 11:36:35 +0100

    Add cart component and Products component to test order store in vuex

---
+++ frontend/src/components/Products.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="wrapper">
+    <div class="products">
+      <div class="buttons" v-for="product in products" :key="product._id">
+        <p>{{ product.title }}</p>
+        <button @click="addToCart(product)">add to cart</button>
+      </div>
+    </div>
+    <Cart />
+  </section>
+</template>
+
+<script>
+import Cart from "@/components/Cart.vue";
+
+export default {
+  components: { Cart },
+
+  computed: {
+    products() {
+      return this.$store.getters["products/getProducts"];
+    }
+  },
+
+  methods: {
+    addToCart(product) {
+      this.$store.dispatch("order/updateOrder", product);
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch("products/getProducts");
+    /*  this.$store.dispatch("user/login", {
+      "email": "customer@example.com",
+      "password": "password"
+    }); */
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 100%;
+  .products {
+    display: flex;
+    flex-wrap: wrap;
+    .buttons {
+      margin: 1rem;
+    }
+  }
+}
+</style>
