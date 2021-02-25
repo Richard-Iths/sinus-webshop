4fb9d6a0b5780f1564d687e9c76b0409ca22de7b
Hatzz <git@hatzz.io>
2021-02-25 11:58:07 +0100

    Add form component with corresponding inputs

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="register">
+    <Form>
+      <div class="col-2">
+        <label for="email">e-mail</label>
+        <input
+          type="text"
+          v-model="user.email"
+          name="email"
+        />
+      </div>
+      <div class="col-2">
+        <label for="password">password</label>
+        <input type="password" v-model="password" name="password">
+      </div>
+      <div class="col-2">
+        <label for="name">name</label>
+
+        <input
+          type="text"
+          v-model="user.name"
+          name="name"
+        />
+      </div>
+
+      <div class="col-2">
+        <label for="street">street</label>
+        <input
+          type="text"
+          v-model="user.address.street"
+          name="street"
+        />
+      </div>
+      <div class="col-2">
+        <label for="zip">zip code</label>
+
+        <input
+          type="text"
+          v-model="user.address.zip"
+          name="zip"
+        />
+      </div>
+
+      <div class="col-2">
+        <label for="city">city</label>
+        <input
+          type="text"
+          v-model="user.address.city"
+          name="city"
+        />
+      </div>
+      <Button class="col-2" value="register" @click="register"/>
+    </Form>
+  </div>
+</template>
+
+<script>
+import Form from "@/components/Form.vue";
+import Button from '@/components/Button.vue';
+export default {
+  components: {
+    Form,
+    Button
+  },
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+        name: "",
+        address: {
+          city: "",
+          zip: "",
+          street: "",
+        }
+      }
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  margin: 0 auto;
+}
+button {
+  margin-left: calc(0.2rem + 2px);
+}
+</style>
